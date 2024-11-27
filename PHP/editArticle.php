62f4068867307825230da4716710014ad0f30af8
Marc Jersey Castro <marcjerseycastro@gmail.com>
2024-11-27 21:43:43 +0800

    clean up privacy policy

--- PHP/editArticle.php
+++ PHP/editArticle.php
@@ -5,18 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="../CSS/createArticle.css">
 
 
     <!-- FONTS AWESOME CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="../CSS/createArticle.css">
 </head>
 
 
 <body>
-    <?php include 'header.php'; ?>
+    <?php include("header.php"); ?>
 
     <main>
         <h1>Create new article</h1>
@@ -98,7 +99,8 @@
 
 
     </main>
-    <?php include 'footer.php' ?>
+
+    <?php include("footer.php") ?>
 </body>
 
 </html>
