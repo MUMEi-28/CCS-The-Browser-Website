01f168170089f26bc236c6da51a9a4e8399fac07
Marc Jersey Castro <marcjerseycastro@gmail.com>
2024-12-02 20:14:40 +0800

    Revert "Merge branch 'main' of https://github.com/MUMEi-28/CCS-The-Browser-Website"
    This reverts commit a9ab21cf0ef5e554a9a0dc301360d94632e805f4, reversing
    changes made to d25ec04060b7b38fd1718c750acf34f6b31eedd9.

--- PHP/Sections/News.php
+++ PHP/Sections/News.php
@@ -1,23 +1,3 @@
-<?php
-
-include("../LogInRegister/php/config.php");
-
-// Get the database || USED TO MAKE SURE WHICH ARTICLE ID THE SITE WILL GO TO
-$sql = "SELECT * FROM articles";
-$result = $con->query($sql);
-/* $row = $result->fetch_assoc();
- */
-
-$row = "";
-$articles = [];
-
-// Fetch all articles into the array
-while ($row = $result->fetch_assoc()) {
-    $articles[] = $row;
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <?php
@@ -34,7 +14,7 @@ while ($row = $result->fetch_assoc()) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="../CSS/main.css">
+    <link rel="stylesheet" href="../../CSS/Sections/SectionsIndex.css">
     <!--     <link rel="stylesheet" href="../CSS/main.css">
  -->
 
@@ -49,27 +29,31 @@ while ($row = $result->fetch_assoc()) {
 
 
 <body>
-    <?php include("header.php"); ?>
+    <?php include("../header.php"); ?>
     <main>
 
         <section class="headliner">
-                    <section class="container one" id="pop-out" data-id="<?php echo htmlspecialchars($articles[0]['artID']); ?>">
-                        <div class="news-item">
-                            <section class="headline">
+            <section class="container one">
+                <div class="news-item">
+                    <section class="headline">
 
-                                <h2><?php echo htmlspecialchars(substr($articles[0]['artHeadline'], 0, 100)) . '...'; ?></h2>
+                        <h2>LOREM IPSUM DOLOR VERY VERY LONG HEADLINE</h2>
 
-                            </section>
-                            <section class="main-news">
-                                <p>
-                                    <?php echo htmlspecialchars(substr($articles[0]['artContent'], 0, 150)) . '...'; ?>
-                                </p>
-                            </section>
-                        </div>
-
-                        <img src="Images/testIMG-Landscape.jpg" alt="Main News Image">
                     </section>
-                </section>
+                    <section class="main-news">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere eget mauris
+                            vel
+                            convallis.
+                            Sed lectus lacus, bibendum et magna tincidunt, dapibus pulvinar metus.
+                        </p>
+                    </section>
+                </div>
+
+                <img src="../../Assets/testIMG-Landscape.jpg" alt="Main News Image">
+
+            </section>
+        </section>
 
         <hr>
 
@@ -77,7 +61,7 @@ while ($row = $result->fetch_assoc()) {
             <section class="container two">
                 <div class="editorial-item">
 
-                    <img src="../Assets/testIMG-Landscape.jpg" alt="Main News Image">
+                    <img src="../../Assets/testIMG-Landscape.jpg" alt="Main News Image">
                     <div style="flex: 1;">
                         <section class="headline">
                             <h2>LOREM IPSUM DOLOR VERY VERY LONG HEADLINE</h2>
@@ -96,7 +80,7 @@ while ($row = $result->fetch_assoc()) {
             <section class="container two">
                 <div class="editorial-item">
 
-                    <img src="../Assets/testIMG-Landscape.jpg" alt="Main News Image">
+                    <img src="../../Assets/testIMG-Landscape.jpg" alt="Main News Image">
                     <div style="flex: 1;">
                         <section class="headline">
                             <h2>LOREM IPSUM DOLOR VERY VERY LONG HEADLINE</h2>
@@ -115,7 +99,7 @@ while ($row = $result->fetch_assoc()) {
             <section class="container two">
                 <div class="editorial-item">
 
-                    <img src="../Assets/testIMG-Landscape.jpg" alt="Main News Image">
+                    <img src="../../Assets/testIMG-Landscape.jpg" alt="Main News Image">
                     <div style="flex: 1;">
                         <section class="headline">
                             <h2>LOREM IPSUM DOLOR VERY VERY LONG HEADLINE</h2>
@@ -135,7 +119,7 @@ while ($row = $result->fetch_assoc()) {
 
     </main>
 
-    <?php include("footer.php"); ?>
+    <?php include("../footer.php"); ?>
 </body>
 
 </html>
