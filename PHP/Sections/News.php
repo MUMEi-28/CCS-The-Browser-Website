16f77f9fb5e11e70c0190c80f41185009a26cfb5
Marc Jersey Castro <marcjerseycastro@gmail.com>
2024-12-02 20:15:13 +0800

    Revert "konting edit sa news.php"
    This reverts commit 754e41974dbc94c34c96986336b7dd792fd52755.

--- PHP/Sections/News.php
+++ PHP/Sections/News.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-    $logoPath = "../Assets/LOGO-AY-24-25/3-WHITE-HEADER-LOGO-BANNER-TYPE.png";
 
-    if (file_exists($logoPath) && !is_dir($logoPath)) {
-        echo '<img src="' . htmlspecialchars($logoPath) . '" alt="Banner" class="header-logo">';
-    } else {
-        echo '<img src="' . htmlspecialchars("Assets/LOGO-AY-24-25/3-WHITE-HEADER-LOGO-BANNER-TYPE.png") . '" alt="Banner" class="header-logo">';
-    }
-    ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,8 +15,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-        
 </head>
 
 
